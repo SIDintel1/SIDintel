ee5ff86eb5ca6e2918d030c22b2c2ada9d218a6d
SIDintel1 <powersgtr@outlook.com>
2025-02-28 22:34:54 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Welcome to SIDIntel</h1>
+            <p>Your source for strategic intelligence and global insights.</p>
+        </div>
+    );
+}
+
+export default App;
