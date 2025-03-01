287882bc5703fb602f56d3ae0722bb47ba1628c2
SIDintel1 <powersgtr@outlook.com>
2025-02-28 22:47:15 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+// Updated to trigger Vercel build
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
